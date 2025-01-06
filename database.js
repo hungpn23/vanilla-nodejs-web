9c0ac6b02071d14723c3adeebd30214509f1016d
Phạm Ngọc Hùng <hungpn23@gmail.com>
2025-01-06 14:29:36 +0700

    add ui, mock database and 2 posts and login routes

---
+++ database.js
@@ -0,0 +1,35 @@
+export const USERS = [
+  {
+    id: 1,
+    name: "Ngoc Hung",
+    username: "ngochung",
+    password: "123123",
+  },
+  {
+    id: 2,
+    name: "Pham Ngoc",
+    username: "phamngoc",
+    password: "123123",
+  },
+];
+
+export const POSTS = [
+  {
+    id: 1,
+    userId: 1,
+    title: "Sed at nisl bibendum, posuere.",
+    body: "Nam eleifend erat quis nisl pharetra eleifend. Sed vitae aliquam magna. Maecenas a erat et ipsum ultrices rhoncus. Phasellus quis dolor at orci fringilla sodales non vitae urna. Aliquam quis ultrices mi.",
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "Vestibulum finibus tristique felis at.",
+    body: "Suspendisse potenti. In hac habitasse platea dictumst. Sed imperdiet mauris enim, vitae semper ex egestas quis. Pellentesque id lectus eget libero finibus hendrerit. Aliquam arcu tortor, interdum convallis odio id, elementum vestibulum lacus. Vestibulum nec mi lacus.",
+  },
+  {
+    id: 3,
+    userId: 1,
+    title: "Proin odio mi, eleifend at.",
+    body: "Duis posuere aliquet condimentum. Nulla eget sollicitudin lorem, sit amet placerat risus. In semper, mi id consequat gravida, risus ligula condimentum velit, ac mattis eros ipsum non lectus",
+  },
+];
--- server.js
+++ server.js
@@ -1,23 +1,54 @@
+import { POSTS, USERS } from "./database.js";
 import { MiniExpress } from "./mini-express.js";
 
 const server = new MiniExpress();
 
+// ---------------- FILES ROUTES ---------------- //
 server.route("GET", "/", (_req, res) => {
-  res.status(200).file("./public/index.html", "text/html");
+  return res.status(200).file("./public/index.html", "text/html");
 });
 
 server.route("GET", "/styles.css", (_req, res) => {
-  res.status(200).file("./public/styles.css", "text/css");
+  return res.status(200).file("./public/styles.css", "text/css");
 });
 
 server.route("GET", "/scripts.js", (_req, res) => {
-  res.status(200).file("./public/scripts.js", "text/javascript");
+  return res.status(200).file("./public/scripts.js", "text/javascript");
 });
 
-server.route("POST", "/login", (_req, res) => {
-  res
-    .status(400)
-    .json({ message: "Route haveroute has not been implemented yet" });
+// ---------------- JSON ROUTES ---------------- //
+server.route("GET", "/api/posts", (_req, res) => {
+  const posts = POSTS.map((post) => {
+    const user = USERS.find((user) => user.id === post.userId);
+
+    post.author = user?.name;
+    return post;
+  });
+  return res.status(200).json(posts);
+});
+
+server.route("POST", "/api/login", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString("utf-8");
+  });
+
+  req.on("end", () => {
+    body = JSON.parse(body);
+    const { username, password } = body;
+    const user = USERS.find(
+      (user) => user.username === username && user.password === password,
+    );
+
+    if (!user)
+      return res.status(401).json({ error: "Invalid username or password" });
+
+    return res.status(200).json({ message: "Login successfully" });
+  });
+});
+
+server.route("GET", "/api/user", (req, res) => {
+  return res.status(400).json({ message: "method not implemented yet" });
 });
 
 server.listen(3002, () => {
